41e2ba18e09570469e2b57b1331bcd521e175d8c
Rammyres <rammyres@gmail.com>
2017-06-02 21:54:02 -0300

    correação de bug, questões 16 e 17

--- lista1/questão16.c
+++ lista1/questão16.c
@@ -1,32 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(void){
 	
-	int n, i, par, impar;
+	srand(time(NULL));
+
+	int i, par=0, impar=0;
 	
 	printf("Para evitar entrar os números vou gerar uma lista aleatória de 20 números: ");
 	int lista[20];
 
-	for (i = 1; i <= 20; i++){
+	for (i = 0; i <= 19; i++){
 	     lista[i]=rand()%10000;
 	     printf("%d ", lista[i]);	
 	}
+	
 	printf("\nVerificando a paridade: ");
 
-	for (i = 1; i <= n; i++){
+	for (i = 0; i <= 19; i++){
 	   if (lista[i]%2==0){
 	      printf("O numero %d é par\n", lista[i]);
 	      par++;
-
 	   }
-           else {
-	    printf("O número %d é ímpar\n", lista[i]);
-	    impar++
+       else {
+	      printf("O número %d é ímpar\n", lista[i]);
+	     impar++;
 	   }
     }
 
-    printf("Existem %d números pares e %d números impares", par, impar);
+    printf("Existem %d números pares e %d números impares\n", par, impar);
 
 	return 0;
 }
--- lista1/questão17.c
+++ lista1/questão17.c
@@ -1,34 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
 
 int main(void){
-	
-	int n, i, par, impar;
+
+	srand(time(NULL));
+
+	int n, i, par=0, impar=0;
 	
 	printf("Para evitar entrar os números vou gerar uma lista aleatória de números: ");
 	printf("Digite quantos números serão avaliados: ");
 	scanf("%d", &n);
 	int lista[n];
 
-	for (i = 1; i <= n; i++){
+	for (i = 1; i < n; i++){
 	     lista[i]=rand()%10000;
 	     printf("%d ", lista[i]);	
 	}
+
 	printf("\nVerificando a paridade: ");
 
-	for (i = 1; i <= n; i++){
+	for (i = 0; i < n; i++){
 	   if (lista[i]%2==0){
 	      printf("O numero %d é par\n", lista[i]);
 	      par++;
-
 	   }
-           else {
-	    printf("O número %d é ímpar\n", lista[i]);
-	    impar++
+       else {
+	      printf("O número %d é ímpar\n", lista[i]);
+	     impar++;
 	   }
     }
 
-    printf("Existem %d números pares e %d números impares", par, impar);
+    printf("Existem %d números pares e %d números impares\n", par, impar);
 
 	return 0;
 }
